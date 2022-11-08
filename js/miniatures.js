63f82756202ba41461fd74c9b564271eff17d690
kiqir <sasha.sushenchov@gmail.com>
2022-11-08 18:26:53 +0500

    Update miniatures.js

--- js/miniatures.js
+++ js/miniatures.js
@@ -1,16 +1,16 @@
 import {getRandomPhoto} from './data.js';
 import {usersIds} from './util.js';
-const picturesTemplate = document.querySelector("#picture").content.querySelector(".picture");
+const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const userPictures = () => Array.from({length: usersIds}, getRandomPhoto);
 const fragment = document.createDocumentFragment();
 userPictures.forEach(() =>{
-   const imgItem = picturesTemplate.cloneNode(true);
-   const img = imgItem.querySelector(".picture__img");
-   const likes = imgItem.querySelector(".picture__likes");
-   const comments = imgItem.querySelector(".picture__comments");
-   img.src = userPictures.url;
-   likes.textContent = userPictures.likes;
-   comments.textContent = userPictures.comment;
-   fragment.appendChild(imgItem);
+  const imgItem = picturesTemplate.cloneNode(true);
+  const img = imgItem.querySelector('.picture__img');
+  const likes = imgItem.querySelector('.picture__likes');
+  const comments = imgItem.querySelector('.picture__comments');
+  img.src = userPictures.url;
+  likes.textContent = userPictures.likes;
+  comments.textContent = userPictures.comment;
+  fragment.appendChild(imgItem);
 });
 
